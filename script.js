d45ca7e276bf02fa2de170cf71daf75dae668af8
Prince Roy <princeku.ray143@gmail.com>
2022-08-02 20:09:38 +0530

    create element

--- script.js
+++ script.js
@@ -33,7 +33,6 @@ document.addEventListener('keydown', function(e) {
 });
 
 console.log(document.documentElement);
-console.log(document.head);
 console.log(document.body);
 
 document.querySelector('.header');
@@ -45,3 +44,19 @@ const allButtons = document.getElementsByTagName('button');
 console.log(allButtons);
 
 document.getElementsByClassName('btn');
+
+// Creating and inserting elements
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+//message.textContent = 'We use cookied for improved functionality and analytics';
+message.innerHTML =
+    'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+const header = document.querySelector('.header');
+//header.prepend(message);
+header.append(message);
+//header.append(message.cloneNode(true));
+
+header.before(message);
+header.after(message);
