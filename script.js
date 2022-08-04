0675179695aa8bc5b05edbd6f4988e6a0fa0365c
Prince Roy <princeku.ray143@gmail.com>
2022-08-05 00:51:19 +0530

    create tabbed component

--- script.js
+++ script.js
@@ -86,6 +86,31 @@ document.querySelector('.nav__links').addEventListener('click', function(e) {
     }
 });
 
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', e => {
+    const clicked = e.target.closest('.operations__tab');
+
+    // Guard clause
+    if (!clicked) return;
+
+    tabs.forEach(t => t.classList.remove('operations__tab--active'));
+
+    tabsContent.forEach(content =>
+        content.classList.remove('operations__content--active')
+    );
+
+    clicked.classList.add('operations__tab--active');
+
+    // Activate content area
+    document
+        .querySelector(`.operations__content--${clicked.dataset.tab}`)
+        .classList.add('operations__content--active');
+});
+
 ///////////////////////////////////////////
 ///////////////////////////////////////////
 //////////////////////////////////////////
@@ -208,4 +233,29 @@ document.querySelector('.nav__links').addEventListener('click', function(e) {
 // document.querySelector('.nav').addEventListener('click', function(e) {
 //     this.style.backgroundColor = randomColor();
 //     console.log('NAV', e.target);
+// });
+
+// const h1 = document.querySelector('h1');
+
+// // Going downwards: Child
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// h1.firstElementChild.style.color = 'white';
+// h1.lastElementChild.style.color = 'orangered';
+
+// // Going upwards: parents
+// console.log(h1.parentNode);
+
+// h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+// // Going sideways: siblings
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+
+// // All siblings
+// console.log(h1.parentElement.children);
+
+// [...h1.parentElement.children].forEach(el => {
+//     el !== h1 && (el.style.transform = 'scale(0.5)');
 // });
