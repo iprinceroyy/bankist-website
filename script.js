1f69bd1024e9b9ca6015bd890944abb9e21ee427
Prince Roy <princeku.ray143@gmail.com>
2022-08-05 17:13:27 +0530

    create sticky navigation

--- script.js
+++ script.js
@@ -130,6 +130,14 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+// Sticky navigation
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener('scroll', function(e) {
+    if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+    else nav.classList.remove('sticky');
+});
+
 ///////////////////////////////////////////
 ///////////////////////////////////////////
 //////////////////////////////////////////
