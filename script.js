6c90ff258456cdfaa6802e98bd91c0df1a6ba18f
Prince Roy <princeku.ray143@gmail.com>
2022-08-06 23:21:00 +0530

    update lazy img loading func

--- script.js
+++ script.js
@@ -196,17 +196,23 @@ const imgTargets = document.querySelectorAll('img[data-src]');
 
 const loadImg = function(entries, observer) {
     const [entry] = entries;
-    console.log(entry);
 
     if (!entry.isIntersecting) return;
 
     // Replace src with data-src
     entry.target.src = entry.target.dataset.src;
+
+    entry.target.addEventListener('load', () => {
+        entry.target.classList.remove('lazy-img');
+    });
+
+    observer.unobserve(entry.target);
 };
 
 const imgObserver = new IntersectionObserver(loadImg, {
     root: null,
     threshold: 0,
+    rootMargin: '+200px',
 });
 
 imgTargets.forEach(img => {
