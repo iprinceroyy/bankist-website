00d60b1ea5e08fc4b16dde4af98e92cb18b97177
Prince Roy <princeku.ray143@gmail.com>
2022-08-06 22:49:26 +0530

    create lazy loading image func

--- script.js
+++ script.js
@@ -191,6 +191,28 @@ allSections.forEach(sec => {
     sec.classList.add('section--hidden');
 });
 
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function(entries, observer) {
+    const [entry] = entries;
+    console.log(entry);
+
+    if (!entry.isIntersecting) return;
+
+    // Replace src with data-src
+    entry.target.src = entry.target.dataset.src;
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+    root: null,
+    threshold: 0,
+});
+
+imgTargets.forEach(img => {
+    imgObserver.observe(img);
+});
+
 ///////////////////////////////////////////
 ///////////////////////////////////////////
 //////////////////////////////////////////
