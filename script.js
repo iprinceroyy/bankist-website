0d2809a0067072a5c364f6c139fa078f1846ab17
Prince Roy <princeku.ray143@gmail.com>
2022-08-07 15:42:55 +0530

    create slider func

--- script.js
+++ script.js
@@ -219,6 +219,46 @@ imgTargets.forEach(img => {
     imgObserver.observe(img);
 });
 
+// Slider
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+let currentSlide = 0;
+const maxSlide = slides.length;
+
+// const slider = document.querySelector('.slider');
+// slider.style.transform = 'scale(0.4) translateX(-800px)';
+// slider.style.overflow = 'visible';
+
+// 0%, 100%, 200%, 200%
+
+const goToSlide = currentSlide => {
+    slides.forEach(
+        (slide, i) =>
+        (slide.style.transform = `translateX(${100 * (i - currentSlide)}%)`)
+    );
+    // currSlide = 1: -100%, 0%, 100%, 200%
+};
+goToSlide(0);
+// Next slide
+const nextSlide = () => {
+    if (currentSlide === maxSlide - 1) currentSlide = 0;
+    else currentSlide++;
+
+    goToSlide(currentSlide);
+};
+
+const prevSlide = () => {
+    if (currentSlide === 0) currentSlide = maxSlide - 1;
+    else currentSlide--;
+
+    goToSlide(currentSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+
 ///////////////////////////////////////////
 ///////////////////////////////////////////
 //////////////////////////////////////////
